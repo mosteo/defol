575dda3d9da63030dccbc5468d6a18809e204cb0
Alejandro R. Mosteo <amosteo@unizar.es>
2026-03-02 09:53:48 +0100

    Fix encoding of "·"

--- src/defol.adb
+++ src/defol.adb
@@ -1399,7 +1399,7 @@ package body Defol is
             & "[" & To_GB (Acum_Processed) & "/" & To_GB (Acum_Size) & "GB]"
             & "[files:" & Trim (Candidates_Processed'Image) & "/" & Trim (Candidates_Count'Image) & "]"
             & "[size:" &
-               Trim (Last_Progress_Size'Image) & "·" &
+               Trim (Last_Progress_Size'Image) & Simple_Logging.U ("·") &
                Trim (Natural'(Sizes_Processed)'Image) & "/" &
                Trim (Sizes_With_Pairs'Image) & "]"
             & "[dup:" & Trim (Dupes'Image) & "/" & To_GB (Duped) & "GB]"
